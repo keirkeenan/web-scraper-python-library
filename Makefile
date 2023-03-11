2041d2e0a5f7eb1f87e2a934865251b4ed83dbdb
keir <89931114+keirkeenan@users.noreply.github.com>
2023-03-11 16:22:17 -0500

    intial CI/CD setup

---
+++ Makefile
@@ -0,0 +1,100 @@
+#########
+# BUILD #
+#########
+develop:  ## install dependencies and build library
+	python3 -m pip install -e .[develop]
+
+build:  ## build the python library
+	python3 setup.py build build_ext --inplace
+
+install:  ## install library
+	python3 -m pip install .
+
+#########
+# LINTS #
+#########
+lint:  ## run static analysis with pylint
+	python3 -m black --check main.py
+	python3 -m pylint main.py
+
+# Alias
+lints: lint
+
+format:  ## run autoformatting with black
+	python3 -m black main.py
+
+# alias
+fix: format
+
+# check:  ## check assets for packaging
+# 	check-manifest -v
+
+# # Alias
+# checks: check
+
+# annotate:  ## run type checking
+# 	python -m mypy ./example_project_python
+
+#########
+# TESTS #
+#########
+# test: ## clean and run unit tests
+# 	python -m pytest -v example_project_python/tests
+
+# coverage:  ## clean and run unit tests with coverage
+# 	python -m pytest -v example_project_python/tests --cov=example_project_python --cov-branch --cov-fail-under=75 --cov-report term-missing
+
+# # Alias
+# tests: test
+test: ## clean and run unit tests with coverage
+	python3 tests.py
+
+###########
+# VERSION #
+###########
+show-version:
+	bump2version --dry-run --allow-dirty setup.py --list | grep current | awk -F= '{print $2}'
+
+patch:
+	bump2version patch
+
+minor:
+	bump2version minor
+
+major:
+	bump2version major
+
+########
+# DIST #
+########
+dist-build:  # Build python dist
+	python setup.py sdist bdist_wheel
+
+dist-check:
+	python -m twine check dist/*
+
+dist: clean build dist-build dist-check  ## Build dists
+
+publish:  # Upload python assets
+	echo "would usually run python -m twine upload dist/* --skip-existing"
+
+#########
+# CLEAN #
+#########
+deep-clean: ## clean everything from the repository
+	git clean -fdx
+
+clean: ## clean the repository
+	rm -rf .coverage coverage cover htmlcov logs build dist *.egg-info .pytest_cache
+
+############################################################################################
+
+# Thanks to Francoise at marmelab.com for this
+.DEFAULT_GOAL := help
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+print-%:
+	@echo '$*=$($*)'
+
+.PHONY: develop build install lint lints format fix check checks annotate test coverage show-coverage tests show-version patch minor major dist-build dist-check dist publish deep-clean clean help
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-'''This function gets the html from the website'''
+"""This function gets the html from the website"""
 
 
 def get_html(company_name, product_name, page_number):
@@ -23,14 +23,14 @@ def get_html(company_name, product_name, page_number):
         url = url_product + url_page
 
         # download the html
-        req = requests.get(url)
+        req = requests.get(url, timeout=60)
         html = req.text
 
         return html
 
     elif company_name == "walmart":
         # build the url
-        url_product = f"https: // www.walmart.com / search?q = {product_name}"
+        url_product = f"https://www.walmart.com/search?q={product_name}"
         url_page = f"&page={page_number}&affinityOverride=default"
         url = url_product + url_page
         ac1 = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,"
@@ -49,7 +49,7 @@ def get_html(company_name, product_name, page_number):
         }
 
         # download the html
-        req = requests.get(url, headers=headers)
+        req = requests.get(url, headers=headers, timeout=60)
         html = req.text
 
         return html
@@ -73,7 +73,7 @@ def get_html(company_name, product_name, page_number):
         }
 
         # download the html
-        req = requests.get(url, headers=headers)
+        req = requests.get(url, headers=headers, timeout=60)
         html = req.text
 
         return html
@@ -82,7 +82,7 @@ def get_html(company_name, product_name, page_number):
         return "Something went wrong. Please try again."
 
 
-'''This is the parse_itemprice function that will parse the price of the item'''
+"""This is the parse_itemprice function that will parse the price of the item"""
 
 
 def parse_itemprice(text):
@@ -92,13 +92,13 @@ def parse_itemprice(text):
     text = text.replace(",", "")
     start = text.find("$")
     end = text.find(".")
-    price_str = text[start + 1: end]
-    price_str += text[end: end + 3]
+    price_str = text[start + 1 : end]
+    price_str += text[end : end + 3]
     price = float(price_str)
     return price
 
 
-'''eBay web scraper'''
+"""eBay web scraper"""
 
 
 def scrape_ebay(product_name):
@@ -110,12 +110,7 @@ def scrape_ebay(product_name):
         time.sleep(random.randint(1, 6))
 
         # get the html & process it
-        soup = BeautifulSoup(
-            get_html(
-                "ebay",
-                product_name,
-                page_number),
-            "html.parser")
+        soup = BeautifulSoup(get_html("ebay", product_name, page_number), "html.parser")
 
         # loop over the items in the page
         tags_items = soup.select(".s-item")
@@ -166,7 +161,7 @@ def scrape_ebay(product_name):
         return "Failed to create JSON file for eBay data."
 
 
-'''Walmart web scraper'''
+"""Walmart web scraper"""
 
 
 def scrape_walmart(product_name):
@@ -185,8 +180,7 @@ def scrape_walmart(product_name):
         soup = BeautifulSoup(html, "html.parser")
 
         # loop over the items in the page
-        products = soup.find_all(
-            "div", class_="mb1 ph1 pa0-xl bb b--near-white w-25")
+        products = soup.find_all("div", class_="mb1 ph1 pa0-xl bb b--near-white w-25")
 
         for product in products:
             try:
@@ -196,8 +190,7 @@ def scrape_walmart(product_name):
             except AttributeError:
                 name = None
             try:
-                price = product.find("div",
-                                     {"data-automation-id": "product-price"})
+                price = product.find("div", {"data-automation-id": "product-price"})
                 price = price.find("span", class_="w_iUH7").text
                 price = parse_itemprice(price)
             except AttributeError:
@@ -236,7 +229,7 @@ def scrape_walmart(product_name):
         return "Failed to create JSON file for Walmart data."
 
 
-'''Amazon web scraper'''
+"""Amazon web scraper"""
 
 
 def scrape_amazon(product_name):
@@ -263,7 +256,8 @@ def scrape_amazon(product_name):
         for product in products:
             try:
                 name = product.find(
-                    "span", class_="a-size-base-plus a-color-base a-text-normal").text
+                    "span", class_="a-size-base-plus a-color-base a-text-normal"
+                ).text
             except AttributeError:
                 name = None
             try:
@@ -311,7 +305,7 @@ def scrape_amazon(product_name):
         return "Failed to create JSON file for Amazon data."
 
 
-'''Main function that takes in the product name and company name'''
+"""Main function that takes in the product name and company name"""
 
 
 def main(product_name, company_name):
